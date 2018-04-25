b45e3e89ff4becc398df5d27e8a35942524b4269
Sonty Visuthikraisee <sontyv@berkeley.edu>
2018-04-25 13:46:36 -0700

    wrote cpp code to generate coordinates of boxes

---
+++ generateCubes.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include <fstream>
+
+#include "CGL/include/CGL/vector3D.h"
+#include "CGL/include/CGL/vector2D.h"
+
+using std::min;
+using std::max;
+using std::swap;
+using std::vector;
+using namespace std;
+using namespace CGL;
+
+void mazeboxCoords(Vector3D box_dimensions, string filename) {
+
+	string line;
+	ifstream mazeFile(filename);
+
+	int maze_w = 0;
+	int maze_h = 0;
+
+	std::vector< std::vector<char> > arr;
+
+	if (mazeFile.is_open()) {
+		while (getline(mazeFile,line)) {
+			if (line == "") {
+				break;
+			}
+			cout << line << '\n';
+			arr.push_back(std::vector<char>());
+			for (unsigned int i = 0; i < line.length(); ++i) {
+				char c = line[i];
+				arr[maze_h].push_back(c);
+			}
+			maze_h += 1;
+		}
+		mazeFile.close();
+	} else {
+		cout << "Unable to open file"; 
+	}
+
+	maze_w = arr[0].size();
+	//std::vector<Vector3D> boxes = std::vector<Vector3D>();
+
+	ofstream coordinates ("coordintes.txt");
+	if (coordinates.is_open()) {
+		for (int r = 0; r < maze_h; ++r) {
+			for (int c = 0; c < maze_w; ++c) {
+
+				if (arr[r][c] == '1') {
+					float x, y, z;
+					x = r * box_dimensions.x + (box_dimensions.x / 2.0);
+					y = c * box_dimensions.y + (box_dimensions.y / 2.0);
+					z = box_dimensions.z / 2.0;
+					//boxes.push_back(Vector3D(x, y, z));
+					coordinates << x << ", " << y << ", " << z << "\n";
+				}
+			}
+		}
+
+		coordinates.close();	
+	} else {
+		cout << "Unable to open file"; 
+	}
+
+}
+
+
+int main(int argc, char *argv[]) {
+	//cout << argv[1] << endl;
+	if (argc != 2) {
+		cout << "This function requires 1 paramter: the name of the file we are reading in" << endl;
+		return 0;
+	}
+	mazeboxCoords(Vector3D(3, 3, 3), argv[1]);
+	return 0;
+}
+
+
+
---
+++ mazegen.py
@@ -0,0 +1,33 @@
+from random import shuffle, randrange
+ 
+def make_maze(w = 16, h = 8):
+    vis = [[0] * w + [1] for _ in range(h)] + [[1] * (w + 1)]
+    ver = [["100"] * w + ['1'] for _ in range(h)] + [[]]
+    hor = [["111"] * w + ['1'] for _ in range(h + 1)]
+ 
+    def walk(x, y):
+        vis[y][x] = 1
+ 
+        d = [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]
+        shuffle(d)
+        for (xx, yy) in d:
+            if vis[yy][xx]: continue
+            if xx == x: hor[max(y, yy)][x] = "100"
+            if yy == y: ver[y][max(x, xx)] = "000"
+            walk(xx, yy)
+ 
+    walk(randrange(w), randrange(h))
+ 
+    s = ""
+    for (a, b) in zip(hor, ver):
+        s += ''.join(a + ['\n'] + b + ['\n'])
+    return s
+
+
+def write_to_file(text):
+    file = open("maze.txt", "w")
+    file.write(text)
+    file.close()
+ 
+if __name__ == '__main__':
+    write_to_file(make_maze())
